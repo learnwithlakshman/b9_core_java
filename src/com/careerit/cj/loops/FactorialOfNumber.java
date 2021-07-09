7feac226fb709793350f9a995506fbd9b4401326
avulalakshman <learnwithlakshman@gmail.com>
2021-07-09 07:03:57 +0530

    Changed for loops to while to find factorial

--- src/com/careerit/cj/loops/FactorialOfNumber.java
+++ src/com/careerit/cj/loops/FactorialOfNumber.java
@@ -9,10 +9,10 @@ public class FactorialOfNumber {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the num: ");
 		int num = sc.nextInt();
-
 		int res = 1;
-		for (int i = 2; i <= num; i++) {
-			res *= i;
+		int i = 2;
+		while(i<=num) {
+			res *= i++;
 		}
 		System.out.println("Factorial of " + num + " is " + res);
 		sc.close();
